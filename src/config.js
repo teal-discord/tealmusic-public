d5571b08de7d36a7a9bd1674be10ec1582068d2d
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2024-09-15 13:13:02 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  OWNER_IDS: ["663221211664482356", "188363246695219201"],
+  PATREON: "https://www.patreon.com/join/lorenzo132",
+  SUPPORT_SERVER: "https://discord.gg/gjDymvVmR8",
+
+  INVITE: {
+    TEAL: "https://discord.com/api/oauth2/authorize?client_id=972795104525975622&permissions=3492864&scope=applications.commands%20bot",
+    GREY: "https://discord.com/api/oauth2/authorize?client_id=940296562976886895&permissions=3492864&scope=applications.commands%20bot",
+    RED: "https://discord.com/api/oauth2/authorize?client_id=1169994012757864448&permissions=3492864&scope=applications.commands%20bot",
+    PINK: "https://discord.com/api/oauth2/authorize?client_id=1238787501435650048&permissions=3492864&scope=applications.commands%20bot",
+    KITSUNE: "https://discord.com/api/oauth2/authorize?client_id=768967985326456874&permissions=3492864&scope=applications.commands%20bot",
+  },
+  IDs: {
+    TEAL: "972795104525975622",
+    GREY: "940296562976886895",
+    PINK: "1238787501435650048",
+  },
+  VOTE: "https://top.gg/bot/972795104525975622/vote",
+  DEFAULT_PREFIX: "-",
+  BOT_EMOJI: "TEAL", // [TEAL, GREY, RED, PINK ]
+
+  SPOTIFY: {
+    CLIENT_ID: "",
+    CLIENT_SECRET: "",
+  },
+
+  AUDIO_NODES: [
+    {
+      id: "node 5950X - 1",
+      host: "",
+      port: 2333,
+      authorization: "",
+      secure: false,
+      retryAmount: 6969420,
+      retryDelay: 10000,
+      requestTimeout: 10000,
+    },
+//   {
+//      id: "node 5950x - 2",
+//      host: "",
+//      port: 2333,
+//      authorization: "",
+//      secure: false,
+//      retryAmount: 6969420,
+//      retryDelay: 10000,
+//      requestTimeout: 10000,
+//   },
+  ],
+
+  PRESENCE: {
+    STATUS: "online", // [online, idle, dnd, invisible]
+    TYPE: "LiSTENING", // [PLAYING | LISTENING | WATCHING | COMPETING | CUSTOMSTATUS]
+    TEXT: "/help",
+  },
+
+  COLORS: {
+    BLUE: "#0066ff",
+    RedPink: "#ff2052",
+    ElectricPurple: "#bf00ff",
+    NeonGreen: "#39ff14",
+    RoyalBlue: "#4169e1",
+    Lavender: "#e6e6fa",
+    Coral: "#ff7f50",
+  },
+};
